80dd08c550f5628758266d19eabb42c66f35c80b
lordzuzen <lordzuzen@gmail.com>
2019-07-21 20:01:39 +0200

    Added Exception Control

--- springboot-service-pokeapi/src/main/java/com/alea/springboot/app/pokeapi/controller/PokemonController.java
+++ springboot-service-pokeapi/src/main/java/com/alea/springboot/app/pokeapi/controller/PokemonController.java
@@ -3,8 +3,10 @@ package com.alea.springboot.app.pokeapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.alea.springboot.app.pokeapi.entity.pokemon.Pokemon;
 import com.alea.springboot.app.pokeapi.service.pokemon.IPokemonService;
@@ -14,19 +16,31 @@ public class PokemonController {
 
 	@Autowired
 	IPokemonService pokemonService;
-	
+
 	@GetMapping("/highestPokemons")
 	public List<Pokemon> findHighestPokemons() {
-		return pokemonService.getHighestPokemons();
+		try {
+			return pokemonService.getHighestPokemons();
+		} catch (Exception exc) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Request");
+		}
 	}
-	
+
 	@GetMapping("/heaviestPokemons")
 	public List<Pokemon> findHeaviestPokemons() {
-		return pokemonService.getHeaviestPokemons();
+		try {
+			return pokemonService.getHeaviestPokemons();
+		} catch (Exception exc) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Request");
+		}
 	}
-	
+
 	@GetMapping("/moreBaseExperiencePokemons")
 	public List<Pokemon> findMoreBaseExperiencePokemons() {
-		return pokemonService.getMoreBaseExperiencePokemons();
+		try {
+			return pokemonService.getMoreBaseExperiencePokemons();
+		} catch (Exception exc) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bad Request");
+		}
 	}
 }
